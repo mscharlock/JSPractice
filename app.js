72aca7ed68d7b28cb31bd24bea1aa759936c7df9
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-06-09 14:47:56 -0700

    Added more constructors

--- app.js
+++ app.js
@@ -30,4 +30,48 @@ function Store(salesToday, salesYesterday, mostPopularItem) {
 }
 
 var sears = new Store(200.96, 1400.45, 'lawnmowers');
-var target = new Store(19678.01, 12345.87, 'goldfish crackers'); 
+var target = new Store(19678.01, 12345.87, 'goldfish crackers');
+
+
+function Key(toWhat, alsoOpens) {
+  this.toWhat = toWhat;
+  this.alsoOpens = alsoOpens;
+}
+
+var carKey = new Key('car', 'garage');
+var houseKey = new Key('house', 'garage');
+var gardenKey = new Key('house', 'turns off alarm system');
+
+function Cake(frosting, fruit, color, flavor) {
+  this.frosting = frosting;
+  this.fruit = fruit;
+  this.color = color;
+  this.flavor = flavor;
+}
+
+var peachCake = new Cake('lemon', true, 'peach', 'peach');
+var kirschCake = new Cake('chocolate', true, 'brown', 'cherries and chocolate');
+var fruitCake = new Cake('none', true, 'multicolored', 'fruits/nuts');
+
+function Pony(fast, cute, hungry) {
+  this.fast = fast;
+  this.cute = cute;
+  this.hungry = hungry;
+}
+
+var lulubelle = new Pony(true, true, true);
+var buttercup = new Pony(false, false, true);
+var cocolala = new Pony(false, true, true);
+
+function Salad(type, dressing, cost, nuts) {
+  this.type = type;
+  this.dressing = dressing;
+  this.cost = cost;
+  this.nuts = nuts;
+}
+
+var cobb = new Salad('Bob\'s famous cobb salad', 'ranch', 9.95, false);
+var threeBean = new Salad('Three Bean Salad', 'none', 2.99, false);
+var walnut = new Salad('Walnut Chicken salad', 'creamy balsamic', 14.32, true);
+
+Salad.prototype.eat = console.log('You ate a salad!');
