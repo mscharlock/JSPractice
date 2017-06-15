ceb8abc385fdca58142aa7e34e140467e551eb6f
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-06-15 13:29:53 -0700

    Create app.js
    Practicing more silly objects to make sure I know the constructor pattern

--- app.js
+++ app.js
@@ -75,3 +75,75 @@ var threeBean = new Salad('Three Bean Salad', 'none', 2.99, false);
 var walnut = new Salad('Walnut Chicken salad', 'creamy balsamic', 14.32, true);
 
 Salad.prototype.eat = console.log('You ate a salad!');
+
+
+function Dress(buttons, color, skirtType) {
+  this.buttons = buttons; 
+  this.color = color; 
+  this.skirtType = skirtType;
+}; 
+
+var eveningGown = new Dress(2, 'blue', 'fitted'); 
+var sunDress = new Dress(0, 'red polka dots', 'A-line'); 
+var businessSuit = new Dress(5, 'tweed', 'pencil skirt'); 
+
+function Bird(from, color, age, name) {
+  this.from = from; 
+  this.color = color; 
+  this.age = age; 
+  this.name = name; 
+}
+
+var parrokeet = new Bird('the pet store', 'green', 0.5, 'Mr. Feathers'); 
+var blueBird = new Bird('outdoors', 'blue', 3, 'none'); 
+var ostrich = newBird('Australia', 'brown', 2, 'Fluffy'); 
+var moa = newBird('a mysterious island', 'white', 'old', 'Indiana'); 
+
+function Fish(hasfins, likesBloodworms, type) {
+  this.hasfins = hasfins; 
+  this.likeBloodworms = likesBloodworms; 
+  this.type = type; 
+}  
+
+var angelfish = new Fish(true, true, 'Calico Angelfish');
+var goldfish = new Fish(true, false, 'Googly-Eyed Goldfish'); 
+var discus = new Fish(true, false, 'Blue Discus'); 
+var zebra = new Fish(true, true, 'Zebrafish'); 
+var goby = new Fish(true, false, 'Bluebacked goby'); 
+
+function Wedding(venue, expensive, chickenOrFish, centerpieces, colorScheme, numberOfBridesmaids) {
+  this.venue = venue; 
+  this.expensive = expensive; 
+  this.chickenOrFish = chickenOrFish; 
+  this.centerpieces = centerpieces; 
+  this.colorScheme = colorScheme; 
+  this.numberOfBridesmaids = numberOfBridesmaids; 
+} 
+
+var beach = new Wedding('beach', false, 'chicken', 25, 'yellow and blue', 4); 
+var katysWedding = new Wedding('barn, but a classy one', false, 'fish', 20, 'brown and gold', 9);
+var castleWedding = new Wedding('castle', true, 'chicken and fish', 54, 'silver and pink', 15); 
+var eloping = new Wedding('Venice', true, 'neither', 0, 'none', 0); 
+                               
+//Practicing Obj Literals//
+var shirt = {
+  sleeves: 2, 
+  buttons: 0, 
+  hasStripes: false, 
+  goodForClubbing: false, 
+  matchesPants: false
+}
+
+var calendar = {
+  days: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], 
+  months: ['January', 'February', 'March', 'April'], 
+  newMoons: 1, 
+  eclipses: false 
+}
+
+var chicken = {
+  fried: true, 
+  baked: false, 
+  tasty: true, 
+  tasteslike: 'chicken'
+}
